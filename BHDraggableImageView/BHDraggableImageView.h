5e277f4d8e11e717135c3eaed69d546293c46406
Josh Parnham <joshparnham@gmail.com>
2015-04-07 18:28:18 +1000

    Import AppKit instead of Foundation (AppKit itself imports Foundation)

--- BHDraggableImageView/BHDraggableImageView.h
+++ BHDraggableImageView/BHDraggableImageView.h
@@ -6,7 +6,7 @@
 //  Copyright 2011  All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 /// NSImageView where the image can be dragged to any 
 /// other image source as a file
